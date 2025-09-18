1122accb01180df1a37b5b7b2a80b318104194b7
Lana <elf_lana@vk.com>
2025-09-18 19:37:51 +0700

    fix deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/',
-    buildAssetsDir: 'assets',
+    baseURL: process.env.NODE_ENV === 'production'
+        ? process.env.NUXT_APP_BASE_URL
+            ? process.env.NUXT_APP_BASE_URL : '/'
+        : './',
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
